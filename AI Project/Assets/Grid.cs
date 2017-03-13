ce46718e0dc9756322e936ea01e354fc6beeddac
sidneydegeus <sidneydegeus@hotmail.com>
2017-03-13 20:13:23 +0100

    added a* with an unoptimized version

--- AI Project/Assets/Grid.cs
+++ AI Project/Assets/Grid.cs
@@ -32,10 +32,31 @@ public class Grid : MonoBehaviour {
                     * (y * nodeDiameter + nodeRadius);
 
                 bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint); 
+                grid[x, y] = new Node(walkable, worldPoint, x, y); 
 
             }
         }
+    }
+
+    public List<Node> GetNeighbours(Node node) {
+        List<Node> neighbours = new List<Node>();
+
+        // check all nodes around the node
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
     }
 
     public Node NodeFromWorldPoint(Vector3 worldPosition) {
@@ -49,12 +70,19 @@ public class Grid : MonoBehaviour {
         return grid[x, y]; 
     }
 
+
+    public List<Node> path;
     private void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
         if (grid != null) {
             foreach (Node node in grid) {
                 Gizmos.color = (node.walkable) ? Color.white : Color.red;
+                if (path != null) {
+                    if (path.Contains(node)) {
+                        Gizmos.color = Color.black;
+                    }
+                }
                 Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - .1f));
             }
         }
--- AI Project/Assets/Node.cs
+++ AI Project/Assets/Node.cs
@@ -6,9 +6,22 @@ public class Node {
 
     public bool walkable;
     public Vector3 worldPosition;
+    public int gridX, gridY;
 
-    public Node(bool _walkable, Vector3 _worldPosition) {
+    public int gCost; // weight of node next to current node (10 horizontal, vertical and 14 diagonally)
+    public int hCost; // expected weight to target position
+    public Node parent;
+
+    public Node(bool _walkable, Vector3 _worldPosition, int _gridX, int _gridY) {
         walkable = _walkable;
         worldPosition = _worldPosition;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+    public int fCost { // summed up weight
+        get {
+            return gCost + hCost;
+        }
     }
 }
