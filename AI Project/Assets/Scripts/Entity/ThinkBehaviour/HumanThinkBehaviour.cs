93a44c8534e5c3c0cdddb85fa03c2afe70484bff
JordanM1123 <jordan_munk@hotmail.com>
2017-06-24 14:34:33 +0200

    Fuzzy added

--- AI Project/Assets/Scripts/Entity/ThinkBehaviour/HumanThinkBehaviour.cs
+++ AI Project/Assets/Scripts/Entity/ThinkBehaviour/HumanThinkBehaviour.cs
@@ -11,6 +11,43 @@ public class HumanThinkBehaviour : IThinkBehaviour {
         human = _human;
     }
 
+    public double FuzzyGetDesirability(int hungerVar, double moneyVar)
+    {
+        FuzzyModule fuzzyModule = new FuzzyModule();
+
+        FuzzyVariable hunger = fuzzyModule.CreateFLV("Hunger");
+        FzSet No_Hunger = hunger.AddLeftShoulderSet("No_Hunger", 0, 3, 6);
+        FzSet Hunger = hunger.AddTriangularSet("Hunger", 6, 9, 12);
+        FzSet Very_Hunger = hunger.AddRightShoulderSet("Very_Hunger", 12, 15, 20);
+
+        FuzzyVariable AmmoStatus = fuzzyModule.CreateFLV("Money");
+        FzSet Poor = AmmoStatus.AddLeftShoulderSet("Poor", 0, 5, 10);
+        FzSet Normal = AmmoStatus.AddTriangularSet("Normal", 10, 50, 100);
+        FzSet Rich = AmmoStatus.AddRightShoulderSet("Rich", 100, 500, 1000);
+
+        FuzzyVariable Desirability = fuzzyModule.CreateFLV("Desirability");
+        FzSet Undesirable = Desirability.AddLeftShoulderSet("Undesirable", 0, 25, 50);
+        FzSet Desirable = Desirability.AddTriangularSet("Desirable", 25, 50, 75);
+        FzSet Very_Desirable = Desirability.AddRightShoulderSet("Very_Desirable", 50, 75, 100);
+
+        fuzzyModule.AddRule(new FzAND(No_Hunger, Poor), Undesirable);
+        fuzzyModule.AddRule(new FzAND(No_Hunger, Normal), Undesirable);
+        fuzzyModule.AddRule(new FzAND(No_Hunger, Rich), Undesirable);
+        fuzzyModule.AddRule(new FzAND(Very_Hunger, Poor), Very_Desirable);
+        fuzzyModule.AddRule(new FzAND(Very_Hunger, Normal), Very_Desirable);
+        fuzzyModule.AddRule(new FzAND(Very_Hunger, Rich), Undesirable);
+        fuzzyModule.AddRule(new FzAND(Hunger, Poor), Desirable);
+        fuzzyModule.AddRule(new FzAND(Hunger, Normal), Desirable);
+        fuzzyModule.AddRule(new FzAND(Hunger, Rich), Undesirable);
+
+        fuzzyModule.Fuzzify("Hunger", hungerVar);
+        fuzzyModule.Fuzzify("Money", moneyVar);
+
+        double value = fuzzyModule.DeFuzzify("Desirability", FuzzyModule.DefuzzifyMethod.MaxAV);
+
+        return value;
+    }
+
     public Action Process() {
         if (human.LastHitBy != null && human.Target != human.LastHitBy) {
             if (human.Think.CurrentAction().GetType() == typeof(Defending) || human.Think.CurrentAction().GetType() == typeof(SeekTarget)) {
@@ -20,7 +57,11 @@ public class HumanThinkBehaviour : IThinkBehaviour {
             human.Target = human.LastHitBy.transform;
             return human.Think.CurrentAction().GetType() != typeof(Defending) ? new Defending(human) : null;
         }
-        if (human.Stats.Hunger > 5 && human.Stats.Money < 50) {
+
+        //Debug.Log("Fuzzy Logic Desirability: " + FuzzyGetDesirability(human.Stats.Hunger, human.Stats.Money));
+
+        //if (human.Stats.Hunger > 5 && human.Stats.Money < 50) {
+        if (FuzzyGetDesirability(human.Stats.Hunger, human.Stats.Money) > 85) {
             if (human.Think.CurrentAction().GetType() != typeof(SeekTarget)) {
                 //FollowpathAction followpathAction = new FollowpathAction(parasite);
                 SeekTarget seekTarget = new SeekTarget(human);
